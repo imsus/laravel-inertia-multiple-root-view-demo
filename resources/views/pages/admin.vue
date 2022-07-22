1882d08bd75a3bd627d03d61512dea1915c1601a
Imam Susanto <abc.imams@gmail.com>
2022-07-22 10:32:06 +0700

    Nambahin navigasi di halaman buat pindah antar root view

--- resources/views/pages/admin.vue
+++ resources/views/pages/admin.vue
@@ -9,6 +9,10 @@ defineProps<{
 
 <template layout>
 	<div class="min-h-screen flex items-center justify-center">
-        <h1 class="text-7xl">pages/admin.vue</h1>
+        <div class="text-center">
+            <h1 class="text-7xl">pages/admin.vue</h1>
+            <a class="text-3xl font-medium mt-12 inline-block underline text-blue-600" href="/">To Landing</a>
+            <div class="mt-3"><strong>Note:</strong> Navigating with different root view should use <code>&lt;a&gt;</code> tag instead of <code>&lt;Link&gt;</code></div>
+        </div>
     </div>
 </template>
--- resources/views/pages/welcome.vue
+++ resources/views/pages/welcome.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { Link } from '@inertiajs/inertia-vue3'
+
 defineProps<{
 	versions: {
 		php: string
@@ -9,6 +11,10 @@ defineProps<{
 
 <template layout>
 	<div class="min-h-screen flex items-center justify-center">
-        <h1 class="text-7xl">pages/welcome.vue</h1>
+        <div class="text-center">
+            <h1 class="text-7xl">pages/welcome.vue</h1>
+            <a class="text-3xl font-medium mt-12 inline-block underline text-blue-600" href="/admin">To Admin</a>
+            <div class="mt-3"><strong>Note:</strong> Navigating with different root view should use <code>&lt;a&gt;</code> tag instead of <code>&lt;Link&gt;</code></div>
+        </div>
     </div>
 </template>
